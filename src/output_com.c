5952d580d00abca07a6c0d7d411d57198d54fdc2
Fahmida Munni <bsse1604@iit.du.ac.bd>
2026-02-04 22:28:30 +0600

    .

--- src/output_com.c
+++ src/output_com.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "metrics.h"
+
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define BLUE "\033[1;34m"
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+
+
 
 
 //ED
